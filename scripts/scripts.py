a322fdeb49b580ff375806cf175ab4c86255f3a8
kahuna-flipper <kkrishnan@ucsd.edu>
2023-04-17 19:31:41 -0700

    dashboard chart routing

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -9,7 +9,7 @@ def strain_summary():
     total_strains = []
     for specie in species:
         comp_strains = []
-        temp_summary = pd.read_csv('./static/'+specie+'/metadata/project_summary.csv')
+        temp_summary = pd.read_csv('../static/'+specie+'/metadata/project_summary.csv')
         strains = temp_summary['Strain']
         mods = temp_summary['Modification/Metadata']
         for st in range(0,len(strains)):
