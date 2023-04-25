f39abea2d6ec53dc3eabb029512492eed7d38e1e
kahuna-flipper <kkrishnan@ucsd.edu>
2023-04-24 18:21:59 -0700

    added dashboard extraction scripts

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from flask import Flask, render_template, url_for, jsonify
+from flask import request
+
 species = ['ecoli','pputida','saureus']
 
 def strain_summary():
@@ -9,7 +12,8 @@ def strain_summary():
     total_strains = []
     for specie in species:
         comp_strains = []
-        temp_summary = pd.read_csv('../static/'+specie+'/metadata/project_summary.csv')
+        temp_summary = pd.read_csv(url_for('.static',filename=specie+'/metadata/summary.csv'))
+        #temp_summary = pd.read_csv('../static/'+specie+'/metadata/summary.csv')
         strains = temp_summary['Strain']
         mods = temp_summary['Modification/Metadata']
         for st in range(0,len(strains)):
@@ -20,3 +24,20 @@ def strain_summary():
 
     return strain_summary
 
+
+def strain_summary_json():
+
+    total_strains = strain_summary()
+    out2 = []
+
+    for i in total_strains.index:
+        num_specie = total_strains.loc[i,'Num Strains']
+        specie = i
+
+        out2.append([
+            str(specie),
+            str(num_specie),])
+
+
+    #return jsonify(data=out)
+    return jsonify(data=out2)
