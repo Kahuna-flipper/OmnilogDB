c105fd8a881576fc91369e2ac1e2d58b02c8c51f
Kahuna-flipper <kkrishnan@ucsd.edu>
2023-05-08 13:19:23 -0700

    updated yaxis in straindata

--- application.py
+++ application.py
@@ -2,23 +2,23 @@ from flask import Flask, render_template, url_for, jsonify
 from flask import request
 ''' CONNECTING TO DATABASES '''
 # MYSQL - for metadata
-from ex_config import SQLALCHEMY_DATABASE_URI, MONGO_CNX
-from sqlalchemy import create_engine
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker(bind=engine)
-session = Session()
+#from ex_config import SQLALCHEMY_DATABASE_URI, MONGO_CNX
+#from sqlalchemy import create_engine
+#engine = create_engine(SQLALCHEMY_DATABASE_URI)
+#from sqlalchemy.orm import sessionmaker
+#Session = sessionmaker(bind=engine)
+#session = Session()
 
 # Mongo for growth data
-import pymongo
+#import pymongo
 # Making a Connection with MongoClient
-myclient = pymongo.MongoClient(MONGO_CNX['client'])
+#myclient = pymongo.MongoClient(MONGO_CNX['client'])
 # database
-mydb = myclient[MONGO_CNX['db']]
+#mydb = myclient[MONGO_CNX['db']]
 # collection
-mongo_table= mydb[MONGO_CNX['collection']]
-data = mongo_table.find()
-from phenom_db import *
+#mongo_table= mydb[MONGO_CNX['collection']]
+#data = mongo_table.find()
+#from phenom_db import *
 import pandas as pd
 import scripts
 
@@ -95,6 +95,51 @@ def species():
     #print(specie)
     #specie = 'ecoli'
     return render_template('species.html',specie=specie)
+
+
+
+@app.route('/mainstraindata', methods=['GET'])
+def mainstraindata():
+
+    growth_calls,well_char,well_id,compound_dict = scripts.get_strain_data('ECP120')
+    chart= {'type': 'heatmap','marginTop': 40,'marginBottom': 80,'plotBorderWidth': 1}
+    title= {'text': ''}
+    xAxis= {
+        'categories': well_id,
+        'labels':{'style':{'fontWeight':'bold','fontSize':'2em','fontFamily':'Monospace'}}
+    }
+
+    yAxis= {
+        'categories': well_char,
+        'title': 'null',
+        'reversed': 'true',
+        'labels':{'style':{'fontWeight':'bold','fontSize':'2em','fontFamily':'Monospace'}}
+    }
+
+
+    legend= {
+        'enabled':'false',
+        'align': 'right',
+        'layout': 'vertical',
+        'margin': 0,
+        'verticalAlign': 'top',
+        'y': 1,
+        'symbolHeight': 280
+    }
+
+    series= [{
+        'name': 'Growth(1)/No Growth(0)/Uncertain(0.5)',
+        'borderWidth': 2.5,
+        'borderColor':'#0a000f',
+        'data': growth_calls,
+        'dataLabels': {
+            'enabled': 'false',
+            'color': '#000000',
+        }
+    }]
+
+    return render_template('mainstraindata.html',chartID='container', chart=chart, data=growth_calls,
+                           title=title,legend = legend,xAxis = xAxis,yAxis=yAxis,compound_dict = compound_dict)
 
 
 @app.route('/straindata', methods=['GET'])
